626da8e1848357e972d6d7585460989275aada87
Kulik <ekulik@siblion.ru>
2019-02-22 16:24:38 +0300

    Добавлена конвертация из pdf в текст

--- src/main/java/jar/App.java
+++ src/main/java/jar/App.java
@@ -1,13 +1,21 @@
 package jar;
 
-/**
- * Hello world!
- *
- */
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        File file = new File(ResourceAware.getPath());
+        try (PDDocument document = PDDocument.load(file)) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            System.out.println(text);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
     }
 }
---
+++ src/main/java/jar/ResourceAware.java
@@ -0,0 +1,7 @@
+package jar;
+
+public class ResourceAware {
+    public static String getPath() {
+        return "C:\\Users\\Kulik\\Desktop\\pdf_words_counter\\parser\\resources\\Docker-in-Action.pdf";
+    }
+}
